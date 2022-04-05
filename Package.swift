bbb4cd3ea7fa0710920f6c58ecd92a3b365913be
bankbiz <thaworn@graffity.in>
2022-04-06 01:49:40 +0700

    change ARCloud class name

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GraffityARCloud",
-            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-0.1.2.xcframework.zip",
-            checksum: "7e6aea14849ddbd7c813d0a349b1f9fcf742616635f144db7827ddea5b8af3a6"
+            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-0.1.4.xcframework.zip",
+            checksum: "3b118870e08e62235a533113db58aeec81c5827e7eda1cb56ad743395bcb0d8d"
         ),
     ]
 )
