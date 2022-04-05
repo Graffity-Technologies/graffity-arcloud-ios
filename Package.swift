0b742cffafc0807906743fde7d91365358bcd0f3
bankbiz <thaworn@graffity.in>
2022-04-06 01:41:06 +0700

    remove placeholder code blocks

--- Package.swift
+++ Package.swift
@@ -16,16 +16,10 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-//        .target(
-//            name: "GraffityARCloud",
-//            dependencies: []),
         .binaryTarget(
             name: "GraffityARCloud",
             url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-0.1.2.xcframework.zip",
             checksum: "7e6aea14849ddbd7c813d0a349b1f9fcf742616635f144db7827ddea5b8af3a6"
         ),
-//        .testTarget(
-//            name: "GraffityARCloudTests",
-//            dependencies: ["GraffityARCloud"]),
     ]
 )
--- Tests/GraffityARCloudTests/graffity_arcloud_iosTests.swift
+++ Tests/GraffityARCloudTests/graffity_arcloud_iosTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import graffity_arcloud_ios
 
 final class graffity_arcloud_iosTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(graffity_arcloud_ios().text, "Hello, World!")
-    }
+    
 }
