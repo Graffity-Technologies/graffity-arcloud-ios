0b95b4e4db9616d8fd9a45d32edfd20261bb0615
bankbiz <thaworn@graffity.in>
2022-04-06 01:29:18 +0700

    pre release 0.1.2

---
+++ Tests/GraffityARCloudTests/graffity_arcloud_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import graffity_arcloud_ios
+
+final class graffity_arcloud_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(graffity_arcloud_ios().text, "Hello, World!")
+    }
+}
