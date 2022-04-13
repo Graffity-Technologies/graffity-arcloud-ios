bf1483b48a6de47ac244b8ec440b14abcc849532
bankbiz <thaworn@graffity.in>
2022-04-13 21:46:22 +0700

    tag version 0.2.2

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "GraffityARCloud",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "GraffityARCloud",
+            targets: ["GraffityARCloud"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GraffityARCloud",
+            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-0.2.2.xcframework.zip",
+            checksum: 7f02f0ed171109381bddf895fcc0b2b3a2c2972a3618369dabcc001748208613
+        )
+    ]
+)
