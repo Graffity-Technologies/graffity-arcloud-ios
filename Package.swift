4e11db6ea10131191faa6c80a3bde422deba160d
bankbiz <thaworn@graffity.tech>
2023-06-02 16:19:09 +0700

    tag version 0.8.6

--- Package.swift
+++ Package.swift
@@ -13,22 +13,22 @@ let package = Package(
             targets: ["GraffityARCloud"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.16.0"),
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.2.0"),
+        // .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.16.0"),
+        // .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.2.0"),
     ],
     targets: [
-        .target(
-            name: "GraffityARCloudWrapper",
-            dependencies: [
-                .target(name: "GraffityARCloud"),
-            ],
-            path: "Sources",
-            publicHeadersPath: ""
-        ),
+        // .target(
+        //     name: "GraffityARCloudWrapper",
+        //     dependencies: [
+        //         .target(name: "GraffityARCloud"),
+        //     ],
+        //     path: "Sources",
+        //     publicHeadersPath: ""
+        // ),
         .binaryTarget(
             name: "GraffityARCloud",
-            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-0.8.5.xcframework.zip",
-            checksum: "3db0a63c63e9ab8f9f35054c19528f3b1cca09cda907078dc2000845da728538"
+            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-0.8.6.xcframework.zip",
+            checksum: "6910f902dadedfd81dadb2a6050d4a61274e21ced76b07138587c841a757a8bb"
         )
     ]
 )
