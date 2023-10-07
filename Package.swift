1c47875e0419b94fe52897411fbbacd5c35ce05a
bankbiz <thaworn@graffity.tech>
2023-10-07 23:45:44 +0700

    tag version 1.0.2

--- Package.swift
+++ Package.swift
@@ -12,12 +12,15 @@ let package = Package(
             name: "GraffityARCloud",
             targets: ["GraffityARCloud"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.16.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.2.0"),
+    ],
     targets: [
         .binaryTarget(
             name: "GraffityARCloud",
-            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-1.0.1.xcframework.zip",
-            checksum: "72c146f7b8ed344e346ff68286af450e13eb42d65fffa6928ae8aaf1ca231ca2"
+            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-1.0.2.xcframework.zip",
+            checksum: "d86f5fb05ae002d12a2d77757df9ef8d424044bebf1a9f4c5fb2e2e3a02ec2d1"
         )
     ]
 )
