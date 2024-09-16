a101ec9c79bdbe1ac304d1c070e49f4207d0e4fa
bankbiz <thaworn@graffity.tech>
2024-09-16 18:28:47 +0700

    tag version 3.3.4

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GraffityARCloud",
-            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-3.3.3.xcframework.zip",
-            checksum: "cf59fd553516e20628e9db76cb7fa1dbc490d9ea55170c22ce7f8fa3265a4232"
+            url: "https://graffity-sdk-public.s3.ap-southeast-1.amazonaws.com/iOS/GraffityARCloud-3.3.4.xcframework.zip",
+            checksum: "1b87b0e9716f4e6e3cd6192d6f5137727e4bf8a045bb78672425aae05bce1904"
         )
     ]
 )
